03706fb1df66af7961d285502cd00e23a96f6350
Muda42 <polak.daniel05@outlook.com>
2020-07-01 10:00:57 +0200

    commit

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="gsbuild-Muda42",
+    version="1.0",
+    author="Muda42",
+    author_email="polak.daniel05@outlook.com",
+    description="Compiler for Google's Product Sans font",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Muda42/gsbuild",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
