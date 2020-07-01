23a2414c52474444693a597cdf28d7f221a40c67
Muda42 <polak.daniel05@outlook.com>
2020-07-01 10:21:29 +0200

    commit

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name="gsbuild",
-    version="1.0",
+    version="r1.0",
     author="Muda42",
     author_email="polak.daniel05@outlook.com",
     description="Compiler for Google's Product Sans font",
